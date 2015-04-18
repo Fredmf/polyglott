3dde7a3c308103f4bed47bdc334e38a006729dd3
FredMF <fmfluegge@gmail.com>
2015-04-19 01:05:45 +0200

    Initial Commit

---
+++ polyglott/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  polyglott
+//
+//  Created by Fred Flügge on 19.04.15.
+//  Copyright (c) 2015 Fred Flügge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ polyglottTests/polyglottTests.swift
@@ -0,0 +1,36 @@
+//
+//  polyglottTests.swift
+//  polyglottTests
+//
+//  Created by Fred Flügge on 19.04.15.
+//  Copyright (c) 2015 Fred Flügge. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class polyglottTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
