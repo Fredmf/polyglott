27c08a297bee43edb2971e69d7df1ca8dda647aa
FredMF <fmfluegge@gmail.com>
2015-04-20 00:15:28 +0200

    initial commit, release 1.1

---
+++ polyglott/voiceData.swift
@@ -0,0 +1,13 @@
+//
+//  voiceData.swift
+//  polyglott
+//
+//  Created by Fred Flügge on 19.04.15.
+//  Copyright (c) 2015 Fred Flügge. All rights reserved.
+//
+
+import Cocoa
+
+class voiceData: NSObject {
+
+}
---
+++ polyglott/voiceDoc.swift
@@ -0,0 +1,13 @@
+//
+//  voiceContainer.swift
+//  polyglott
+//
+//  Created by Fred Flügge on 19.04.15.
+//  Copyright (c) 2015 Fred Flügge. All rights reserved.
+//
+
+import Cocoa
+
+class voiceContainer: NSObject {
+
+}
